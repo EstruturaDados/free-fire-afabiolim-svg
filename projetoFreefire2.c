9a32c1cfc492ab70c5bb1a9a6854f3e4817a815e
afabiolim-svg <afabiolim@gmail.com>
2025-11-05 15:56:54 +0000

    printf do painel

--- projetoFreefire2.c
+++ projetoFreefire2.c
@@ -13,3 +13,14 @@ int main() {
     Item mochila[MAX_ITENS];
     int total = 0;
     int opcao;
+
+    do {
+        printf("\n=========CODIGO DA ILHA - NIVEL AVENTUREIRO ==========\n");
+        printf("1. Adicionar item\n");
+        printf("2. Remover item\n");
+        printf("3. Listar itens\n");
+        printf("4. Buscar item por nome\n");
+        printf("5. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar();
