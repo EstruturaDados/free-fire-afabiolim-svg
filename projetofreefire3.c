ef6eea037897bdbb6260808fe85cd7c75e6bb2b3
afabiolim-svg <afabiolim@gmail.com>
2025-11-05 20:41:35 +0000

    commi

--- projetofreefire3.c
+++ projetofreefire3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h> // usar bool, true, false
 
 #define MAX_ITENS 10
 
+// Ordenação
+typedef enum {
+    ORDENAR_NOME = 1,
+    ORDENAR_TIPO,
+    ORDENAR_PRIORIDADE
+} CriterioOrdenacao;
