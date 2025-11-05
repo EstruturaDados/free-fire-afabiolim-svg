e88ffa32db019581ba5fbf69e09d684d96d08b3d
afabiolim-svg <afabiolim@gmail.com>
2025-11-05 20:57:07 +0000

    esruturamochila

--- projetofreefire3.c
+++ projetofreefire3.c
@@ -10,3 +10,12 @@ typedef enum {
     ORDENAR_TIPO,
     ORDENAR_PRIORIDADE
 } CriterioOrdenacao;
+
+
+//estrutura da mochila
+typedef struct {
+    char nome[30];
+    char tipo[20];
+    int quantidade;
+    int prioridade; // 1 (baixa) a 5 (alta)
+} Item;
