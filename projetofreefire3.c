e0b93bf86f60bc90ba81569d774dcc7c662d21a7
afabiolim-svg <afabiolim@gmail.com>
2025-11-06 19:36:57 +0000

    funcao

--- projetofreefire3.c
+++ projetofreefire3.c
@@ -19,3 +19,19 @@ typedef struct {
     int quantidade;
     int prioridade; // 1 (baixa) a 5 (alta)
 } Item;
+
+
+// Função que exibe um item
+void exibirItem(Item it) {
+    printf("Nome: %s\n", it.nome);
+    printf("Tipo: %s\n", it.tipo);
+    printf("Quantidade: %d\n", it.quantidade);
+    printf("Prioridade: %d\n", it.prioridade);
+}
+
+// Adiciona um item na mochila 
+void adicionarItem(Item mochila[], int *total) {
+    if (*total >= MAX_ITENS) {
+        printf(" Mochila cheia! Nao e possivel adicionar mais itens.\n");
+        return;
+    }
